74f1331865de661122b953a8c32d5ec2905d8abb
Max <max@dfinity.org>
2022-06-23 05:38:41 +0200

    Add SNS description for launchpad

---
+++ src/standards/dfinity-internal.ts
@@ -0,0 +1,17 @@
+
+
+/**
+ * Data needed by the SNS frontend dapp for token summaries.
+ */
+export interface SnsSummary {
+  // Authority?  Root canister?
+  logo: string;
+  // Authority?  Root canister?
+  name: string;
+  // Authority?  Root canister?
+  symbol: string;
+  // Authority?  Root canister?
+  url: string;
+  // Description
+  description?: string;
+}
