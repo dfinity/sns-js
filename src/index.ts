fbdb71fb044482047f7e58a4b46898c6b5cd60ef
Max <max@dfinity.org>
2022-06-23 04:55:41 +0200

    ++

--- src/index.ts
+++ src/index.ts
@@ -21,13 +21,22 @@ export public class Sns {
     /**
      * Gets the overall state of the SNS, e.g. has the swap taken place yet?  Is it accepting commitments?
      */
-    async getState(): Promise<{acc}>
+    async getState(): Promise<{acc}> {
+      throw new Error("Unimplemented");
+      // Call: swap canister: get_canister_status()
+    }
 
+    async orderCommitmnet({fromUserPrincipal, icp}): Invoice {
+      throw new Error("Unimplemented");
+    }
     /**
      * Makes a commitment.
      */
-    async commitIcp({icp, userPrincipal, subaccountId}: {icp: whatever, userPrincipal: Principal, subaccountId: whatever}): Promise<void> {
+    async claimCommitment({icp, userPrincipal, subaccountId}: {icp: whatever, userPrincipal: Principal, subaccountId: whatever}): Promise<void> {
       throw new Error("Unimplemented");
+      // Call NNS ledger canister to send funds to an account number that is a function of SNS and user principal.
+      // - Note: Should this be done in sns-js?  Should sns-js instead have a method to give the address to send funds?
+      // Call the SNS swap canister: refresh_buyer_tokens(userPrincipal)
     }
 
     /**
